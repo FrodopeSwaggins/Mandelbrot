6a180c75d5238c7eb2ac7f8222c3de546218a2fe
jorda <jorda@LAPTOP-2JBB4VCN>
2023-04-25 22:21:11 -0700

    Add project files.

---
+++ ComplexPlane.cpp
@@ -0,0 +1,117 @@
+#include "ComplexPlane.h"
+#include <cmath>
+#include <iostream>
+#include <complex>      // std::complex, std::abs
+#include <sstream>
+using namespace std;
+
+ComplexPlane::ComplexPlane(float aspectRatio)
+{
+	m_aspectRatio = aspectRatio;
+	m_view.setSize(BASE_WIDTH, -BASE_HEIGHT * m_aspectRatio);
+	m_view.setCenter(0.0, 0.0);
+	m_zoomCount = 0;
+}
+void ComplexPlane::zoomIn()
+{
+	m_zoomCount++;
+	//Set a local variable for the x size to BASE_WIDTH* (BASE_ZOOM to the m_ZoomCount power)	
+	double x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+	//Set a local variable for the y size to BASE_HEIGHT* m_aspectRatio* (BASE_ZOOM to the m_ZoomCount power)
+	double y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+	m_view.setSize(x, y);
+}
+void ComplexPlane::zoomOut()
+{
+	m_zoomCount--;
+	double x = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+	double y = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+	m_view.setSize(x, y);
+}
+void ComplexPlane::setCenter(Vector2f coord)
+{
+	//setCenter for m_view to the given coordinate
+	m_view.setCenter(coord);
+}
+View ComplexPlane::getView()
+{
+	return m_view;
+
+}
+void ComplexPlane::setMouseLocation(Vector2f coord)
+{
+	//Store the given value in the m_mouseLocation variable
+	m_mouseLocation = coord;
+}
+void ComplexPlane::loadText(Text& text)
+{
+	Vector2f center;
+	center = m_view.getCenter();
+	stringstream ss;
+
+	ss << "Mandelbrot Set" << endl << "Center: (" << center.x << "," << center.y << ")" << endl << "Cursor: (" << m_mouseLocation.x << ", " << m_mouseLocation.y << ")" << endl << "Left - click to Zoom in" << endl << "Right - click to Zoom out" << endl;
+
+	text.setString(ss.str());
+
+}
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
+	double re = coord.x;
+	double im = coord.y;
+	complex<double> c(re, im);
+	int counter = 0;
+	complex<double> z(0, 0);
+	while (abs(z) < 2) {
+		z = z * z + c;
+		counter++;
+		if (counter == MAX_ITER) {
+			return counter;
+		}
+	}
+	return counter;
+
+}
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+	int region = MAX_ITER / 5;
+	int x = count / region;
+
+	if (count == MAX_ITER)
+	{
+		r = 0;
+		g = 0;
+		b = 0;
+		return;
+	}
+	else
+	{
+		switch (x)
+		{
+		case 0:
+			r = 128;
+			g = 0;
+			b = 128 + (count % region) * (127 / region);
+			break;
+		case 1:
+			r = 0;
+			g = 0;
+			b = 128 + (count % region) * (127 / region);
+			break;
+		case 2:
+			r = 0;
+			g = 128 + (count % region) * (127 / region);
+			b = 0;
+			break;
+		case 3:
+			r = 255 - (count % region) * (127 / region);
+			g = 255 - (count % region) * (255 / region);
+			b = 0;
+			break;
+		default:
+			r = 255;
+			g = 0;
+			b = 0;
+			break;
+		}
+	}
+}
---
+++ main.cpp
@@ -0,0 +1,125 @@
+// MandleBot.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "ComplexPlane.h"
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+	float ratio;
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;															////Get resulution
+	ratio = resolution.y / resolution.x;
+
+	RenderWindow window(VideoMode(resolution.x, resolution.y), "Mandelbrot", Style::Default);
+
+	ComplexPlane View(ratio);//rename
+
+	Text text;																									//Text
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP_.ttf");
+	text.setFont(font);
+	text.setCharacterSize(25);
+	text.setPosition(50, 50);
+	text.setFillColor(Color::Red);
+
+	VertexArray background;																						//vertex array
+	background.setPrimitiveType(Points);
+	background.resize(resolution.x * resolution.y);
+
+	enum action { CALCULATING, DISPLAYING };
+	action now = CALCULATING;
+
+
+	// The main game loop
+	while (window.isOpen())
+	{
+		Vector2f coord, vcoord, pixelc;//maybe use?
+		Vector2i mouseP, vmouseP;
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)                                                        //Close for red X 
+			{
+				window.close();
+			}
+			if (event.type == sf::Event::MouseButtonPressed) {
+
+				if (event.mouseButton.button == Mouse::Left) {
+					View.zoomIn();
+
+					coord = window.mapPixelToCoords(Mouse::getPosition(), View.getView());
+					/*mouseP = Mouse::getPosition(window);
+					coord = window.mapPixelToCoords(mouseP);
+					*/
+					View.setCenter(coord);
+					now = CALCULATING;
+
+
+				}
+
+				if (event.mouseButton.button == Mouse::Right) {
+					View.zoomOut();
+					coord = window.mapPixelToCoords(Mouse::getPosition(), View.getView());
+					//mouseP = Mouse::getPosition(window);
+					//coord = window.mapPixelToCoords(mouseP);
+					View.setCenter(coord);
+					now = CALCULATING;
+
+				}
+			}
+
+			if (event.type == sf::Event::MouseMoved) {
+				//vmouseP = Mouse::getPosition(window);
+				//vcoord = window.mapPixelToCoords(vmouseP);
+				vcoord = window.mapPixelToCoords(Mouse::getPosition(), View.getView());
+				View.setMouseLocation(vcoord);
+			}
+
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Escape))                                               //ESC TO CLOSE
+		{
+			window.close();
+		}
+
+
+
+		//Update scene;
+		if (now == CALCULATING) 
+		{
+			for (float j = 0; j < resolution.x; j++) 
+			{
+				for (float i = 0; i < resolution.y; i++) 
+				{
+					background[j + i * resolution.x].position = { (float)j,(float)i };						//pixelWidth,am not sure;
+					float counter = 0;
+					Vector2i coord(j, i);
+					Vector2f worldCord;
+					worldCord = window.mapPixelToCoords(coord, View.getView());
+
+					counter = View.countIterations(worldCord);//fix
+					Uint8 r, g, b;
+					View.iterationsToRGB(counter, r, g, b);
+					background[j + i * resolution.x].color = { r,g,b };
+				}
+			}
+			now = DISPLAYING;
+		}
+
+		//draw
+		//window.setView(View.getView());
+		View.loadText(text);
+		window.clear();
+		window.draw(background);
+		window.draw(text);
+		window.display();
+
+	}
+
+
+}
